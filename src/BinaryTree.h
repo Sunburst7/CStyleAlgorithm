02e44940adb13bcc12ca535a803ffc0d962b6d58
Sunburst7 <1064658281@qq.com>
2024-02-18 09:14:24 +0000

    @update BinaryTree Defination

--- src/BinaryTree.h
+++ src/BinaryTree.h
@@ -5,4 +5,14 @@
  * @LastEditTime : 2024-01-12 14:13:56
  * @Description  : Binary Tree
  */
+#ifndef _BITNODE_H_
+#define _BITNODE_H_
+#include <Core.h>
 
+typedef struct BiTNode{
+    ElemType data;
+    BiTNode* left;
+    BiTNode* right;
+} BiTNode;
+
+#endif
